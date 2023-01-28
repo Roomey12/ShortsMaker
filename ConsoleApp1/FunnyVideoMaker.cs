69114e81914c71e7e5be9035e05dae9475e23921
Oleksandr Lutsenko <oleksandr.l@siteplus.com>
2023-01-29 00:38:36 +0200

    init

---
+++ ConsoleApp1/FunnyVideoMaker.cs
@@ -0,0 +1,65 @@
+﻿using FFmpeg.NET;
+
+namespace ConsoleApp1;
+
+public class FunnyVideoMaker
+{
+    private static int _gameVideoNumber = 1;
+    private const string Fonts = @"C\\:/Windows/fonts/consola.ttf";
+    private readonly Engine _ffmpeg;
+    public FunnyVideoMaker()
+    {
+        _ffmpeg = new Engine();
+    }
+
+    private async Task<double> GetVideoDurationAsync(string videoPath)
+    {
+        var inputFile = new InputFile(videoPath);
+        var metaData = await _ffmpeg.GetMetaDataAsync(inputFile, CancellationToken.None);
+        return metaData.Duration.TotalSeconds;
+    }
+    
+    public async Task SplitFamilyGuyVideoAsync(string videoDirectory, string videoName, string splittedVideoName, string outputDirectory)
+    {
+        var input = @$"{videoDirectory}\{videoName}.mp4";
+        var duration = await GetVideoDurationAsync(input);
+        var step = 59;
+        var startTime = 32;
+        var endTime = startTime + step;
+        var part = 1;
+        while (endTime < duration)
+        {
+            var command1 = @$"-i {input} -ss {startTime} -to {endTime} -vf scale=640:-1,pad=ceil(iw/2)*2:ceil(ih/2)*2 {outputDirectory}\{splittedVideoName}_part_{part}.mp4";
+            await _ffmpeg.ExecuteAsync(command1, CancellationToken.None);
+            Console.WriteLine($"Created {part} video");
+            startTime += step;
+            endTime += step;
+            part++;
+        }
+    }
+
+    public async Task SplitGameVideoAsync(string input)
+    {
+        var output = @"C:\Users\OleksandrLutsenko\Desktop\games_splitted\game_";
+        var duration = await GetVideoDurationAsync(input);
+        var step = 59;
+        var startTime = 0;
+        var endTime = startTime + step;
+        while (endTime < duration)
+        {
+            var command = $@" -i {input} -ss {startTime} -to {endTime} -filter:v ""crop=560:720:360:0,scale=640:-1,pad=ceil(iw/2)*2:ceil(ih/2)*2"" {output}{_gameVideoNumber}.mp4";
+            await _ffmpeg.ExecuteAsync(command, CancellationToken.None);
+            Console.WriteLine($"Created {_gameVideoNumber} video");
+            startTime += step;
+            endTime += step;
+            _gameVideoNumber++;
+        }
+    }
+
+    public async Task MergeVideosAsync(string funnyVideoPath, string gameVideoPath, string outputVideoPath, int part)
+    {
+        var filter = $"vstack,drawtext=fontfile={Fonts}:fontsize=72:fontcolor='yellow':text='PART {part}':bordercolor=black:borderw=5:x=(w-text_w)/2:y=(h-text_h)/2-135";
+        var command = $@"-i {funnyVideoPath} -i {gameVideoPath} -filter_complex ""{filter}"" {outputVideoPath}";
+        await _ffmpeg.ExecuteAsync(command, CancellationToken.None);
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using ConsoleApp1;
+
+Console.WriteLine("Start work");
+
+var videoMaker = new FunnyVideoMaker();
+var random = new Random();
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+await SplitFamilyGuyVideosAsync();
+
+stopwatch.Stop();
+Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+async Task MergeFunnyAndGameVideos(string funnyVideosPath)
+{
+    var funnyFolder = @"C:\Users\OleksandrLutsenko\Desktop\familyguy_splitted\";
+    var funnyVideosCount = Directory.EnumerateFiles(funnyFolder).Count();
+    var gameVideo = GetRandomVideo();
+    for (var i = 1; i <= funnyVideosCount; i++)
+    {
+        //await videoMaker.MergeVideosAsync()
+
+    }
+}
+
+async Task SplitFamilyGuyVideosAsync(string familyGuyDirectory = @"C:\Users\OleksandrLutsenko\Desktop\family_guy_s2",
+                                     string outputDirectory = @"C:\Users\OleksandrLutsenko\Desktop\familyguy_splitted")
+{
+    var fgVideosCount = Directory.EnumerateFiles(familyGuyDirectory).Count();
+    for (var i = 1; i <= fgVideosCount; i++)
+    {
+        await videoMaker.SplitFamilyGuyVideoAsync(familyGuyDirectory, $"s2e{i}",$"s_2_e_{i}", outputDirectory);
+    }
+    
+}
+
+string GetRandomVideo(int videosCount = 83) => $@"C:\Users\OleksandrLutsenko\Desktop\games_splitted\get_{random.Next(1, videosCount)}.mp4";
